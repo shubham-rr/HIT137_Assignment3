bc775b86adab9ed0befb315db9298cb09d865653
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2024-10-18 16:03:04 +0930

    question1: add constants.py

---
+++ question1/constants.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+# ------ APP CONSTANTS ------
+WINDOW_MIN_WIDTH = 1000
+WINDOW_MIN_HEIGHT = 600
+APP_TITLE = 'Object Detector'
+
+# ------ BASE PATHS ------
+BASE_DIR = Path(__file__).resolve().parent
+ASSETS_DIR = BASE_DIR / 'assets'
+
+# ------ IMAGE ASSETS ------
+ABOUT_ICON_PATH = ASSETS_DIR / 'about_icon.png'
+CLOSE_BTN_ICON_PATH = ASSETS_DIR / 'close_btn_icon.png'
+CONTACT_ICON_PATH = ASSETS_DIR / 'contact_icon.png'
+HOME_ICON_PATH = ASSETS_DIR / 'home_icon.png'
+HOME_PATH = ASSETS_DIR / 'home.png'
+INFO_ICON_PATH = ASSETS_DIR / 'infor.png'
+INSTRUCTION_ICON_PATH = ASSETS_DIR / 'insstr.png'
+L_ARROW_ICON_PATH = ASSETS_DIR / 'larrow.png'
+LOGO_PATH = ASSETS_DIR / 'logo.png'
+MENU_ICON_PATH = ASSETS_DIR / 'menu.png'
+R_ARROW_ICON_PATH = ASSETS_DIR / 'rarrow.png'
+SERVICES_ICON_PATH = ASSETS_DIR / 'services_icon.png'
+SETTING_ICON_PATH = ASSETS_DIR / 'settings.png'
+TOGGLE_BTN_ICON_PATH = ASSETS_DIR / 'toggle_btn_icon.png'
+UPDATES_ICON_PATH = ASSETS_DIR / 'updates_icon.png'
+ACCOUNT_ICON_PATH = ASSETS_DIR / 'logo.png' # need to change this
+
+
+
+
+
+
+# ------ OTHER CONSTANTS ------
+
